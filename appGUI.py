32247db59ebb4d0eb0e63e28ee1eceefe896ed10
Carlos Meza <cmeza432@gmail.com>
2021-01-27 16:42:37 -0600

    edited the window label and image

--- appGUI.py
+++ appGUI.py
@@ -1,6 +1,19 @@
-import tkinter as tk
+from tkinter import *
+from tkinter.ttk import *
 
+# Window property where everything is going to be inside of
+window = Tk()
 
-window = tk.Tk()
+######### Window Configurations #########
+# Set window size
+window.geometry("500x500")
+# Set window color
+window.configure(bg='slateblue1')
+# Set the window title
+window.title("Punching Count App")
+# Create image for title label
+windowImage = PhotoImage(file = "windowImage.png")
+# Setting icon of master window
+window.iconphoto(False, windowImage)
 
-window.mainloop()
+mainloop()
